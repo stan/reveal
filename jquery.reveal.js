9d5a824c85afd47f8bd86c1a78467bbb95cd606a
Matt Daly <matt@matthew-daly.com>
2011-11-16 04:41:23 +0000

    small restructuring to improve filesize

--- jquery.reveal.js
+++ jquery.reveal.js
@@ -9,10 +9,10 @@
 (function ($) {
     var defaults = {
         animation: 'fadeAndPop',                    // fade, fadeAndPop, none
-        animationSpeed: 300,                        // how fast animtions are
+        animationSpeed: 250,                        // how fast animations are
         closeOnBackgroundClick: true,               // if you click background will modal close?
         closeOnKey: 27,                             // close on a specific key (27 is the keycode for the escape key)
-        closeOnTimeout: false,                      // close the modal after provided given milliseconds
+        closeOnTimeout: false,                      // close the modal after provided  milliseconds
         dismissModalClass: 'close-reveal-modal',    // the class of a button or element that will close an open modal
         revealedCallback: null,                     // optional callback to run after the modal has revealed (loaded)
         dismissCallback: null                       // optional callback to run after the modal has closed
@@ -25,61 +25,55 @@
 
     $.fn.reveal = function (args) {
         return this.each(function () {
-            var modal   = $(this),
-            topMeasure  = parseInt(modal.css('top')),
-            topOffset   = modal.height() + topMeasure,
-            locked      = false,
-            background  = $('.reveal-modal-bg'),
-            timeout     = null,
-            options     = $.extend({}, defaults, args);
+            var modal = $(this),
+            topMeasure = parseInt(modal.css('top')),
+            topOffset = modal.height() + topMeasure,
+            locked = false,
+            background = $('.reveal-modal-bg'),
+            timeout = null,
+            options = $.extend({}, defaults, args);
 
             // open
             modal.bind('reveal:open', function () {
-                if (background.length == 0)
+                if (background.length === 0)
                     background = $('<div class="reveal-modal-bg" />').insertAfter(modal).fadeTo('fast', 0.8);
 
                 if (!locked) {
                     locked = true;
-                    modal.css({ 'opacity': 0, 'top': $(document).scrollTop() + topMeasure, 'visibility': 'visible' });
 
                     var animations = { 'opacity': 1 };
-                    var css = {};
+                    modal.css({ 'opacity': 0, 'top': $(document).scrollTop() + topMeasure, 'visibility': 'visible' });
 
                     if (options.animation == 'fadeAndPop') {
-                        css.top = $(document).scrollTop() - topMeasure;
+                        modal.css('top', $(document).scrollTop() - topMeasure);
                         animations.top = $(document).scrollTop() + topMeasure + 'px';
                     } else if (options.animation == 'fade') {
                         // nothing
                     } else if (options.animation == 'none') {
-                        css.opacity = 1;
+                        modal.css('opacity', 1);
                         options.animationSpeed = 0;
                     }
-                    modal.css(css);
+
                     background.fadeIn(options.animationSpeed / 2);
                     modal.delay(options.animationSpeed / 2).animate(animations, options.animationSpeed, function () {
                         eval(options.revealedCallback);
                     });
-                }
 
-                // close if the background is clicked
-                if (options.closeOnBackgroundClick) {
-                    background.css({ 'cursor': 'pointer' }).bind('click.modalEvent', function () {
-                        modal.trigger('reveal:close');
-                    });
-                }
+                    // close if the background is clicked
+                    if (options.closeOnBackgroundClick)
+                        background.css('cursor', 'pointer').bind('click', close);
 
-                // close if the specified key is pressed (default escape)
-                if (options.closeOnKey) {
-                    $('body').keyup(function (e) {
-                        if (e.which === options.closeOnKey)
-                            modal.trigger('reveal:close');
-                    });
-                }
+                    // close if the specified key is pressed (default escape)
+                    if (options.closeOnKey) {
+                        $('body').keyup(function (e) {
+                            if (e.which === options.closeOnKey)
+                                close();
+                        });
+                    }
 
-                // close if the x is clicked
-                modal.find('.' + options.dismissModalClass).live('click.modalEvent', function () {
-                    modal.trigger('reveal:close')
-                });
+                    // close if the x is clicked
+                    modal.find('.' + options.dismissModalClass).live('click', close);
+                }
 
                 locked = false;
                 modal.unbind('reveal:open');
@@ -90,8 +84,7 @@
                 if (!locked) {
                     locked = true
 
-                    var animations = { 'opacity': 0 };
-                    var css = { 'top': topMeasure, 'visibility': 'hidden' };
+                    var animations = { 'opacity': 0 }, css = { 'top': topMeasure, 'visibility': 'hidden' };
 
                     if (options.animation == 'fadeAndPop') {
                         animations.top = $(document).scrollTop() - topOffset + 'px';
@@ -102,7 +95,9 @@
                     }
 
                     modal.animate(animations, options.animationSpeed, function () { modal.css(css); });
-                    background.delay(options.animationSpeed).fadeOut(options.animationSpeed, function () { eval(options.dismissCallback); });
+                    background.delay(options.animationSpeed).fadeOut(options.animationSpeed, function () {
+                        eval(options.dismissCallback);
+                    });
                 }
 
                 clearTimeout(timeout);
@@ -114,6 +109,10 @@
 
             if (options.closeOnTimeout)
                 timeout = setTimeout(function () { modal.trigger('reveal:close'); }, options.closeOnTimeout);
+
+            function close() {
+                modal.trigger('reveal:close');
+            }
         });
     };
 })(jQuery);
